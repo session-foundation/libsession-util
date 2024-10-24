3a4eb57b028d34c6b071fda8c68aa2ccd9e75070
Ryan Miller <aerilym@proton.me>
2024-10-25 10:53:57 +1100

    Change body hash to body
    Co-authored-by: Thomas Winget <tewinget@gmail.com>

--- src/blinding.cpp
+++ src/blinding.cpp
@@ -459,15 +459,15 @@ ustring blind15_sign(ustring_view ed25519_sk, std::string_view server_pk_in, ust
 ustring blind_version_sign_request(ustring_view ed25519_sk, uint64_t timestamp, ustring_view method, ustring_view path, std::optional<ustring_view> body) {
     auto [pk, sk] = blind_version_key_pair(ed25519_sk);
 
-    // Signature should be on `TIMESTAMP || METHOD || PATH || BODY_HASH`
+    // Signature should be on `TIMESTAMP || METHOD || PATH || BODY`
     ustring buf;
-    buf.reserve(10 + 6 + path.size() + body.value_or({}).size());
+    buf.reserve(10 + 6 + path.size() + (body ? body->size() : 0));
     buf += to_unsigned_sv(std::to_string(timestamp));
     buf += to_unsigned_sv(method);
     buf += to_unsigned_sv(path);
 
     if (body)
-        buf += blake2b(*body, 64);
+        buf += *body;
 
     return ed25519::sign({sk.data(), sk.size()}, buf);
 }
