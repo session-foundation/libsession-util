beb9106807613e68978ebd0bda9aa2e7d157fa3f
Morgan Pretty <morgan.t.pretty@gmail.com>
2025-03-04 16:47:42 +1100

    Added a couple of unit tests to validate the merge bug fixes

--- tests/test_bugs.cpp
+++ tests/test_bugs.cpp
@@ -69,3 +69,107 @@ TEST_CASE("Dirty/Mutable test case", "[config][dirty]") {
     CHECK(seqno4 == 3);  // The merge *and* change should go into the same message update/seqno
     CHECK(as_set(obs4) == make_set("fakehash1"s, "fakehash2"s, "fakehash3"s));
 }
+
+// There was a bug where if we merge the current config into itself then the current hash would be
+// included in the old_hashes (which would result in clients deleting the current config from the
+// swarm)
+TEST_CASE("Merge existing config into clean state", "[config][merge_existing]") {
+    const auto seed = "0123456789abcdef0123456789abcdef00000000000000000000000000000000"_hexbytes;
+    std::array<unsigned char, 32> ed_pk, curve_pk;
+    std::array<unsigned char, 64> ed_sk;
+    crypto_sign_ed25519_seed_keypair(
+            ed_pk.data(), ed_sk.data(), reinterpret_cast<const unsigned char*>(seed.data()));
+    int rc = crypto_sign_ed25519_pk_to_curve25519(curve_pk.data(), ed_pk.data());
+    REQUIRE(rc == 0);
+
+    REQUIRE(oxenc::to_hex(ed_pk.begin(), ed_pk.end()) ==
+            "4cb76fdc6d32278e3f83dbf608360ecc6b65727934b85d2fb86862ff98c46ab7");
+    REQUIRE(oxenc::to_hex(curve_pk.begin(), curve_pk.end()) ==
+            "d2ad010eeb72d72e561d9de7bd7b6989af77dcabffa03a5111a6c859ae5c3a72");
+    CHECK(oxenc::to_hex(seed.begin(), seed.end()) ==
+          oxenc::to_hex(ed_sk.begin(), ed_sk.begin() + 32));
+
+    session::config::Contacts c1{ustring_view{seed}, std::nullopt};
+    c1.set_name("050000000000000000000000000000000000000000000000000000000000000000", "alfonso");
+    auto [seqno, data, obsolete] = c1.push();
+    CHECK(obsolete == std::vector<std::string>{});
+    c1.confirm_pushed(seqno, "fakehash1");
+    c1.dump();
+    CHECK(!c1.needs_dump());
+    CHECK(!c1.needs_push());
+
+    auto r = c1.merge(std::vector<std::pair<std::string, ustring_view>>{{{"fakehash1", data}}});
+    CHECK(as_set(r) == make_set("fakehash1"s));
+
+    auto old_hashes = c1.old_hashes();
+    CHECK(old_hashes.empty());
+}
+
+// There was a bug where if the current config is in a dirty state and we merge a config which makes
+// the same change we would remain in the dirty state but the merged configs has would be included
+// in old_hashes (which ends up being the same hash the dirty config gets after pushing, resulting
+// in the current config getting deleted from the swarn)
+TEST_CASE("Merge config matching local changse", "[config][merge_matching_dirty]") {
+    const auto seed = "0123456789abcdef0123456789abcdef00000000000000000000000000000000"_hexbytes;
+    std::array<unsigned char, 32> ed_pk, curve_pk;
+    std::array<unsigned char, 64> ed_sk;
+    crypto_sign_ed25519_seed_keypair(
+            ed_pk.data(), ed_sk.data(), reinterpret_cast<const unsigned char*>(seed.data()));
+    int rc = crypto_sign_ed25519_pk_to_curve25519(curve_pk.data(), ed_pk.data());
+    REQUIRE(rc == 0);
+
+    REQUIRE(oxenc::to_hex(ed_pk.begin(), ed_pk.end()) ==
+            "4cb76fdc6d32278e3f83dbf608360ecc6b65727934b85d2fb86862ff98c46ab7");
+    REQUIRE(oxenc::to_hex(curve_pk.begin(), curve_pk.end()) ==
+            "d2ad010eeb72d72e561d9de7bd7b6989af77dcabffa03a5111a6c859ae5c3a72");
+    CHECK(oxenc::to_hex(seed.begin(), seed.end()) ==
+          oxenc::to_hex(ed_sk.begin(), ed_sk.begin() + 32));
+
+    session::config::Contacts c1{ustring_view{seed}, std::nullopt};
+    c1.set_name("050000000000000000000000000000000000000000000000000000000000000000", "alfonso");
+    auto [seqno, data, obsolete] = c1.push();
+    CHECK(obsolete == std::vector<std::string>{});
+    c1.confirm_pushed(seqno, "fakehash1");
+
+    session::config::Contacts c2{ustring_view{seed}, c1.dump()};
+
+    CHECK_FALSE(c2.needs_dump());
+    CHECK_FALSE(c2.needs_push());
+
+    // If the current dirty state matches a merged config we should end up in a clean state
+    c1.set_name("051111111111111111111111111111111111111111111111111111111111111111", "barney");
+    c2.set_name("051111111111111111111111111111111111111111111111111111111111111111", "barney");
+
+    auto [seqno2, data2, obs2] = c2.push();
+
+    REQUIRE(seqno2 == 2);
+    CHECK(obs2 == std::vector{"fakehash1"s});
+    c2.confirm_pushed(seqno2, "fakehash2");
+
+    CHECK(c1.is_dirty());  // already dirty before the merge
+    auto r = c1.merge(std::vector<std::pair<std::string, ustring_view>>{{{"fakehash2", data2}}});
+    CHECK(r == std::vector{{"fakehash2"s}});
+    CHECK(c1.needs_dump());
+
+    CHECK_FALSE(c1.needs_push());  // the merge resulted in the config being identical
+    CHECK_FALSE(c1.is_dirty());
+    CHECK(c1.is_clean());
+
+    // Ensure if there are still changes after the merge we remain dirty
+    c1.set_name("051111111111111111111111111111111111111111111111111111111111111112", "barney2");
+    c1.set_name("051111111111111111111111111111111111111111111111111111111111111113", "barney3");
+    c2.set_name("051111111111111111111111111111111111111111111111111111111111111112", "barney2");
+
+    auto [seqno3, data3, obs3] = c2.push();
+    REQUIRE(seqno3 == 3);
+    CHECK(obs3 == std::vector{"fakehash2"s});
+
+    CHECK(c1.is_dirty());  // already dirty before the merge
+    auto r2 = c1.merge(std::vector<std::pair<std::string, ustring_view>>{{{"fakehash3", data3}}});
+    CHECK(r2 == std::vector{{"fakehash3"s}});
+    CHECK(c1.needs_dump());
+
+    CHECK(c1.needs_push());  // there are still changes after the merge
+    CHECK(c1.is_dirty());
+    CHECK_FALSE(c1.is_clean());
+}
