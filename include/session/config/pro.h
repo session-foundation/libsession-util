7d6d7cb33492d0b9b241e4c6783a093f41852682
doylet <doyle.github.2ifxi@simplelogin.com>
2025-08-08 16:29:31 +1000

    Port encrypt for namespace into libsession

--- include/session/config/pro.h
+++ include/session/config/pro.h
@@ -4,6 +4,7 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
--- include/session/config/user_profile.h
+++ include/session/config/user_profile.h
@@ -7,6 +7,7 @@ extern "C" {
 #include "base.h"
 #include "pro.h"
 #include "profile_pic.h"
+#include "pro.h"
 
 /// API: user_profile/user_profile_init
 ///
