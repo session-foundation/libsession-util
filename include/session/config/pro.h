c03d138df8a2e40f3af5aac7281fab990c5cfb96
doylet <doyle.github.2ifxi@simplelogin.com>
2025-08-21 16:00:26 +1000

    Add helper functions for getting PRO_STATUS from proof standalone from decryption

--- include/session/config/pro.h
+++ include/session/config/pro.h
@@ -11,6 +11,19 @@ extern "C" {
 #include "../export.h"
 #include "../types.h"
 
+typedef enum PRO_STATUS {  // See session::ProStatus
+    PRO_STATUS_NIL,
+    PRO_STATUS_INVALID_PRO_BACKEND_SIG,
+    PRO_STATUS_INVALID_USER_SIG,
+    PRO_STATUS_VALID,
+    PRO_STATUS_EXPIRED,
+} PRO_STATUS;
+
+typedef struct pro_signed_message {
+    span_u8 sig;
+    span_u8 msg;
+} pro_signed_message;
+
 typedef struct pro_proof {
     uint8_t version;
     uint8_t gen_index_hash[32];
@@ -19,7 +32,7 @@ typedef struct pro_proof {
     uint8_t sig[64];
 } pro_proof;
 
-typedef struct pro_pro_config {
+typedef struct pro_config {
     uint8_t rotating_privkey[64];
     pro_proof proof;
 } pro_pro_config;
@@ -37,7 +50,7 @@ typedef struct pro_pro_config {
 /// - `bytes32` -- The 32 byte hash calculated from the proof
 LIBSESSION_EXPORT bytes32 pro_proof_hash(pro_proof const* proof);
 
-/// API: pro/pro_proof_verify
+/// API: pro/pro_proof_verify_signature
 ///
 /// Verify the proof was signed by the `verify_pubkey`
 ///
@@ -49,9 +62,78 @@ LIBSESSION_EXPORT bytes32 pro_proof_hash(pro_proof const* proof);
 ///   parameterised to detect errors about incorrectly sized arrays by the caller.
 ///
 /// Outputs:
-/// - `bytes32` -- The 32 byte hash calculated from the proof
-LIBSESSION_EXPORT bool pro_proof_verify(
+/// - `bool` -- True if verified, false otherwise
+LIBSESSION_EXPORT bool pro_proof_verify_signature(
         pro_proof const* proof, uint8_t const* verify_pubkey, size_t verify_pubkey_len);
+
+/// API: pro/pro_proof_verify_message
+///
+/// Check if the `rotating_pubkey` in the proof was the signatory of the message and signature
+/// passed in. This function throws if an signature is passed in that isn't 64 bytes.
+///
+/// Inputs:
+/// - `proof` -- Proof to verify
+/// - `sig` -- Signature to verify with the `rotating_pubkey`. The signature should have
+///   originally been signed over `msg` passed in.
+/// - `sig_len` -- Length of the signature, should be 64 bytes
+/// - `msg` -- Message that the signature signed over with. It will be verified using the
+///   embedded `rotating_pubkey`.
+/// - `msg_len` -- Length of the message
+///
+/// Outputs:
+/// - `bool` -- True if verified, false otherwise (bad signature, or, invalid arguments).
+LIBSESSION_EXPORT bool pro_proof_verify_message(
+        pro_proof const* proof,
+        uint8_t const* sig,
+        size_t sig_len,
+        uint8_t const* msg,
+        size_t msg_len);
+
+/// API: pro/pro_proof_is_active
+///
+/// Check if the Pro proof is currently entitled to Pro given the `unix_ts` with respect to the
+/// proof's `expiry_unix_ts`
+///
+/// Inputs:
+/// - `proof` -- Proof to verify
+/// - `unix_ts_s` -- The unix timestamp in seconds to check the proof expiry time against
+///
+/// Outputs:
+/// - `bool` -- True if expired, false otherwise
+LIBSESSION_EXPORT bool pro_proof_is_active(pro_proof const* proof, uint64_t unix_ts_s);
+
+/// API: pro/pro_proof_status
+///
+/// Evaluate the status of the pro proof by checking it is signed by the `verify_pubkey`, it has
+/// not expired via `unix_ts_s` and optionally verify that the `signed_msg` was signed by the
+/// `rotating_pubkey` embedded in the proof.
+///
+/// Internally this function calls `pro_proof_verify_signature`, `pro_proof_verify_message` and
+/// optionally `pro_proof_is_active` in sequence. This function fails if an invalidly sized public
+/// key or signature are passed in. They must be 32 and 64 bytes respectively, the appropriate
+/// invalid status will be returned.
+///
+/// Inputs:
+/// - `proof` -- Proof to verify
+/// - `verify_pubkey` -- 32 byte Ed25519 public key of the corresponding secret key to check if
+///   they are the original signatory of the proof.
+/// - `verify_pubkey_len` -- Length of the `verify_pubkey` should be 32 bytes
+///   they are the original signatory of the proof.
+/// - `unix_ts` -- Unix timestamp in seconds to compared against the embedded `expiry_unix_ts`
+///   to determine if the proof has expired or not
+/// - `signed_msg` -- Optionally set the payload to the message with the signature to verify if
+///   the embedded `rotating_pubkey` in the proof signed the given message.
+///
+/// Outputs:
+/// - `status` - The derived status given the components of the message. If `signed_msg` is
+///   not set then this function can never return `PRO_STATUS_INVALID_USER_SIG` from the set of
+///   possible enum values. Otherwise this funtion can return all possible values.
+LIBSESSION_EXPORT PRO_STATUS pro_proof_status(
+        pro_proof const* proof,
+        const uint8_t* verify_pubkey,
+        size_t verify_pubkey_len,
+        uint64_t unix_ts_s,
+        const pro_signed_message* signed_msg);
 
 /// API: pro/pro_verify
 ///
@@ -67,8 +149,8 @@ LIBSESSION_EXPORT bool pro_proof_verify(
 ///
 /// Outputs:
 /// - `bytes32` -- The 32 byte hash calculated from the proof
-LIBSESSION_EXPORT bool pro_pro_verify(
-        pro_pro_config const* pro, uint8_t const* verify_pubkey, size_t verify_pubkey_len);
+LIBSESSION_EXPORT bool pro_config_verify_signature(
+        pro_config const* pro, uint8_t const* verify_pubkey, size_t verify_pubkey_len);
 
 #ifdef __cplusplus
 }  // extern "C"
--- tests/test_config_pro.cpp
+++ tests/test_config_pro.cpp
@@ -1,4 +1,5 @@
 #include <oxenc/hex.h>
+#include <session/config/pro.h>
 #include <sodium/crypto_sign_ed25519.h>
 
 #include <catch2/catch_test_macros.hpp>
@@ -8,7 +9,7 @@ using namespace oxenc::literals;
 
 TEST_CASE("Pro", "[config][pro]") {
     // Setup keys
-    std::array<unsigned char, crypto_sign_ed25519_PUBLICKEYBYTES> rotating_pk, signing_pk;
+    std::array<uint8_t, crypto_sign_ed25519_PUBLICKEYBYTES> rotating_pk, signing_pk;
     session::cleared_uc64 rotating_sk, signing_sk;
     {
         crypto_sign_ed25519_keypair(rotating_pk.data(), rotating_sk.data());
@@ -16,43 +17,100 @@ TEST_CASE("Pro", "[config][pro]") {
     }
 
     // Setup the Pro data structure
-    session::config::ProConfig pro = {};
+    session::config::ProConfig pro_cpp = {};
+    pro_config pro = {};
     {
-        pro.rotating_privkey = rotating_sk;
-        pro.proof.version = 0;
-        pro.proof.rotating_pubkey = rotating_pk;
-        pro.proof.expiry_unix_ts = std::chrono::sys_seconds(1s);
-
+        // CPP
+        pro_cpp.rotating_privkey = rotating_sk;
+        pro_cpp.proof.version = 0;
+        pro_cpp.proof.rotating_pubkey = rotating_pk;
+        pro_cpp.proof.expiry_unix_ts = std::chrono::sys_seconds(1s);
         constexpr auto gen_index_hash =
                 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"_hex_u;
-        static_assert(pro.proof.gen_index_hash.max_size() == gen_index_hash.size());
-        std::memcpy(pro.proof.gen_index_hash.data(), gen_index_hash.data(), gen_index_hash.size());
+        static_assert(pro_cpp.proof.gen_index_hash.max_size() == gen_index_hash.size());
+        std::memcpy(
+                pro_cpp.proof.gen_index_hash.data(), gen_index_hash.data(), gen_index_hash.size());
+
+        // C
+        std::memcpy(pro.rotating_privkey, rotating_sk.data(), rotating_sk.size());
+        pro.proof.version = pro_cpp.proof.version;
+        std::memcpy(pro.proof.rotating_pubkey, rotating_pk.data(), rotating_pk.size());
+        pro.proof.expiry_unix_ts = pro_cpp.proof.expiry_unix_ts.time_since_epoch().count();
+        std::memcpy(pro.proof.gen_index_hash, gen_index_hash.data(), gen_index_hash.size());
     }
 
-    // Do the signing
+    // Generate and write the hashes that are signed by the faux pro backend into the proof
     {
-        static_assert(crypto_sign_ed25519_BYTES == pro.proof.sig.max_size());
-        std::array<uint8_t, 32> hash_to_sign = pro.proof.hash();
+        // Generate the hashes
+        static_assert(crypto_sign_ed25519_BYTES == pro_cpp.proof.sig.max_size());
+        std::array<uint8_t, 32> hash_to_sign_cpp = pro_cpp.proof.hash();
+        bytes32 hash_to_sign = pro_proof_hash(&pro.proof);
+
+        static_assert(hash_to_sign_cpp.size() == sizeof(hash_to_sign));
+        CHECK(std::memcmp(hash_to_sign_cpp.data(), hash_to_sign.data, hash_to_sign_cpp.size()) ==
+              0);
+
+        // Write the signature into the proof
         int sig_result = crypto_sign_ed25519_detached(
-                pro.proof.sig.data(),
+                pro_cpp.proof.sig.data(),
                 nullptr,
-                hash_to_sign.data(),
-                hash_to_sign.size(),
+                hash_to_sign_cpp.data(),
+                hash_to_sign_cpp.size(),
+                signing_sk.data());
+        CHECK(sig_result == 0);
+
+        sig_result = crypto_sign_ed25519_detached(
+                pro.proof.sig,
+                nullptr,
+                hash_to_sign.data,
+                sizeof(hash_to_sign.data),
                 signing_sk.data());
         CHECK(sig_result == 0);
     }
 
-    // Verify the proof
+    // Verify the signature in the proof was signed by the faux pro backend key "signing_sk"
     {
-        CHECK_FALSE(pro.verify(rotating_pk));
-        CHECK(pro.verify(signing_pk));
+        CHECK_FALSE(pro_cpp.verify_signature(rotating_pk));
+        CHECK(pro_cpp.verify_signature(signing_pk));
+
+        CHECK_FALSE(pro_config_verify_signature(&pro, rotating_pk.data(), rotating_pk.size()));
+        CHECK(pro_config_verify_signature(&pro, signing_pk.data(), signing_pk.size()));
+    }
+
+    // Verify expiry
+    {
+        CHECK(pro_cpp.proof.is_active(pro_cpp.proof.expiry_unix_ts));
+        CHECK_FALSE(pro_cpp.proof.is_active(pro_cpp.proof.expiry_unix_ts + 1s));
+
+        CHECK(pro_proof_is_active(&pro.proof, pro.proof.expiry_unix_ts));
+        CHECK_FALSE(pro_proof_is_active(&pro.proof, pro.proof.expiry_unix_ts + 1));
+    }
+
+    // Verify it can verify messages signed with the rotating public key
+    {
+        std::string_view body = "hello world";
+        std::array<uint8_t, crypto_sign_ed25519_BYTES> sig = {};
+        int sign_result = crypto_sign_ed25519_detached(
+                sig.data(),
+                nullptr,
+                reinterpret_cast<const uint8_t*>(body.data()),
+                body.size(),
+                rotating_sk.data());
+        CHECK(sign_result == 0);
+        CHECK(pro_cpp.proof.verify_message(sig, session::to_span(body)));
+        CHECK(pro_proof_verify_message(
+                &pro.proof,
+                sig.data(),
+                sig.size(),
+                reinterpret_cast<const uint8_t*>(body.data()),
+                body.size()));
     }
 
     // Try loading the proof from dict
     session::config::dict good_dict;
     {
         // clang-format off
-        const session::config::ProProof& proof = pro.proof;
+        const session::config::ProProof& proof = pro_cpp.proof;
         good_dict = {
             {"r", std::string(reinterpret_cast<const char *>(rotating_sk.data()), rotating_sk.size())},
             {"p", session::config::dict{
@@ -67,23 +125,23 @@ TEST_CASE("Pro", "[config][pro]") {
 
         session::config::ProConfig loaded_pro = {};
         CHECK(loaded_pro.load(good_dict));
-        CHECK(loaded_pro.rotating_privkey == pro.rotating_privkey);
-        CHECK(loaded_pro.proof.version == pro.proof.version);
-        CHECK(loaded_pro.proof.gen_index_hash == pro.proof.gen_index_hash);
-        CHECK(loaded_pro.proof.rotating_pubkey == pro.proof.rotating_pubkey);
-        CHECK(loaded_pro.proof.expiry_unix_ts == pro.proof.expiry_unix_ts);
-        CHECK(loaded_pro.proof.sig == pro.proof.sig);
-        CHECK(loaded_pro.verify(signing_pk));
+        CHECK(loaded_pro.rotating_privkey == pro_cpp.rotating_privkey);
+        CHECK(loaded_pro.proof.version == pro_cpp.proof.version);
+        CHECK(loaded_pro.proof.gen_index_hash == pro_cpp.proof.gen_index_hash);
+        CHECK(loaded_pro.proof.rotating_pubkey == pro_cpp.proof.rotating_pubkey);
+        CHECK(loaded_pro.proof.expiry_unix_ts == pro_cpp.proof.expiry_unix_ts);
+        CHECK(loaded_pro.proof.sig == pro_cpp.proof.sig);
+        CHECK(loaded_pro.verify_signature(signing_pk));
     }
 
     // Try loading a proof with a bad signature in it from dict
     {
         session::config::dict bad_dict = good_dict;
-        std::array<uint8_t, 64> broken_sig = pro.proof.sig;
+        std::array<uint8_t, 64> broken_sig = pro_cpp.proof.sig;
         broken_sig[0] = ~broken_sig[0];  // Break the sig
 
         // clang-format off
-        const session::config::ProProof& proof = pro.proof;
+        const session::config::ProProof& proof = pro_cpp.proof;
         bad_dict = {
             {"r", std::string(reinterpret_cast<const char *>(rotating_sk.data()), rotating_sk.size())},
             {"p", session::config::dict{
@@ -98,6 +156,6 @@ TEST_CASE("Pro", "[config][pro]") {
 
         session::config::ProConfig loaded_pro = {};
         CHECK(loaded_pro.load(bad_dict));
-        CHECK_FALSE(loaded_pro.verify(signing_pk));
+        CHECK_FALSE(loaded_pro.verify_signature(signing_pk));
     }
 }
