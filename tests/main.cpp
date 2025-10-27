696850f6670add5a272ff018c5de2c233de71969
doylet <doyle.github.2ifxi@simplelogin.com>
2025-10-27 14:59:07 +1100

    Make pro backend server tests off by default, specify URL to let it run

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_session.hpp>
 #include <oxen/log.hpp>
 
-std::string g_test_pro_backend_dev_server_url = "http://127.0.0.1:5000";
+std::string g_test_pro_backend_dev_server_url = "";
 
 int main(int argc, char* argv[]) {
     Catch::Session session;
@@ -20,7 +20,8 @@ int main(int argc, char* argv[]) {
                        "enable oxen log tracing of test cases/sections") |
                Opt(g_test_pro_backend_dev_server_url, "url")["--pro-backend-dev-server-url"](
                        "URL to a SESH_PRO_BACKEND_DEV=1 enabled Session Pro Backend server. Only "
-                       "used if compiled with -D TEST_PRO_BACKEND_WITH_DEV_SERVER=1 support");
+                       "used if compiled with -D TEST_PRO_BACKEND_WITH_DEV_SERVER=1 support. These "
+                       "tests are skipped if not specified.");
 
     session.cli(cli);
 
@@ -46,6 +47,9 @@ int main(int argc, char* argv[]) {
     oxen::log::set_level(
             oxen::log::Cat("testcase"),
             test_case_tracing ? oxen::log::Level::trace : oxen::log::Level::off);
+
+    if (g_test_pro_backend_dev_server_url.empty())
+        fprintf(stdout, "Skipping --pro-backend-dev-server-url tests, no URL specified.\n");
 
     return session.run();
 }
