2d910d7dd32d70bcce8209ee88ead2f91445f3f4
SessionHero01 <SesshioHero01@getsession.org>
2025-11-20 09:07:24 +1100

    Update C header

--- include/session/util.h
+++ include/session/util.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include "export.h"
 #include <uchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /// Truncates an utf-16 encoded string to at most `codepoint_len` codepoints long, taking care to not
 /// truncate in the middle of a surrogate pair.
-size_t utf16_len_for_codepoints(
+LIBSESSION_EXPORT size_t utf16_len_for_codepoints(
     const char16_t *utf16_string,
     size_t utf16_string_len,
     size_t codepoint_len
 );
+
+#ifdef __cplusplus
+}
+#endif
